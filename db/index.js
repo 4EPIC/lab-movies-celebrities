e3b6ef89771b8db4bdd15025c21a3f982f980a78
Eric Esteban <eric_7@hotmail.com>
2021-11-16 14:24:23 +0100

    done

--- db/index.js
+++ db/index.js
@@ -5,17 +5,21 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-movies-celebrities";
+const MONGO_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://4EPIC:cgee81104@cluster0.mbel6.mongodb.net/ovellesnegres?authSource=admin&replicaSet=atlas-4y2vsw-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true";
 
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
---
+++ routes/celebrities.routes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+
+router.get("/create", (req, res) => {
+  res.render("celebrities/new-celebrity");
+});
+
+router.get("/", (req, res) => {
+  Celebrity.find().then((allCelebs) => {
+    res.render("celebrities/celebrities", {
+      allCelebs,
+    });
+  });
+});
+
+router.post("/create", (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then((newCelebrity) => res.redirect("/celebrities"))
+    .catch((err) => res.render("/celebrities/new-celebrity"));
+});
+
+module.exports = router;
